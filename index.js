b73d5066e97491f2f8ba94ecf202eeae29ff50a1
DanaZuazo_Personal <danalaurazuazoortiz@gmail.com>
2026-07-17 08:36:01 -0600

    implementacion del middleware de app-token

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const PORT = process.env.PORT || 5100;
 const connectDB = require('./src/config/database');
 const expedientesRoutes = require('./src/routes/expedientes');
 const usuariosRoutes = require('./src/routes/usuarios');
+const appTokenMiddleware = require('./src/middlewares/appToken');
 
 connectDB();
 
 app.use(express.json());
+app.use(appTokenMiddleware);
 app.use('/api',expedientesRoutes);
 app.use('/api/usuarios',usuariosRoutes);
 app.listen(PORT, () =>{
